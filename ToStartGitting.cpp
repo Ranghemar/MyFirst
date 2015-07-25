fc4bc155916bc90bea576b30bb0c10f0224c500a
Volodymyr T. Sukhoteplyy <V.Sukhoteplyy@ukr.net>
2015-07-25 14:50:50 +0300

    To be trormented by Denis

---
+++ ToStartGitting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int someFunction(int, int);
+
+int main(int argc, char **argv)
+{
+	
+
+cout << "Result = " << someFunction(5,6);
+cout << endl;
+	
+		return 0;
+}
+ 
+
+ int someFunction( int a, int b)
+	{ 
+			int result; 
+			result = a + b;
+		return result;
+	}
